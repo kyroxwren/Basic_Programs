b6bc8b6f649f318857e8bddde0320f8382f9e804
Lebogang Tyosho <lebogang15@icloud.com>
2023-11-01 13:19:36 +0200

    Update Wordle

--- Wordle_Clone/wordle_functions.py
+++ Wordle_Clone/wordle_functions.py
@@ -1,41 +1,57 @@
 import random
-import tkinter
-from tkinter.constants import *
+from termcolor import colored
 
+# --------------------------- main game ----------------------------------
 
 # This will start the game with and option to fill in a word
 def play_game():
-    # tk = tkinter.Tk()
-    # frame = tkinter.Frame(tk, width=500, height=400, name="wordle")
-    # frame.pack()
-    # label = tkinter.Label(frame, text="Welcome to wordle")
-    # label.pack()
-    # button = tkinter.Button(frame, text="Exit", command=tk.destroy)
-    # button.pack(side=BOTTOM)
-    # tk.mainloop()
     game_attempts = 5
     i = 0
+    correct_letters = []
     guessed_words = []
+
+    print("------------------------------")
+    print("Welcome to wordle")
+    print("------------------------------")
 
     while i < game_attempts:
 
         print("------------------------------")
-        print("Welcome to wordle")
-        print("------------------------------")
         pc_rd_wd = random.choice(random_words)
         guess_word = input("Please write a word: ").lower()
-        checked = word_check(guess_word, pc_rd_wd)
-        guessed_words.append(checked)
-        for letter in guessed_words:
+
+        guessed_words.append(guess_word)
+
+        checked = word_check(pc_rd_wd, guess_word)
+        correct_letters.append(checked)
+
+        print("------------------------------")
+        print()
+        print("These the correct letters")
+        for letter in correct_letters:
             print(letter)
-        correct_word(pc_rd_wd, guess_word)
+        print()
+        print("These are your words you have guessed")
+        for letter in guessed_words:
+            print(colored(letter,"red"))
+        print()
+
+        correct = correct_word(pc_rd_wd, guess_word)
+        if correct:
+            print("----------------------------------------------------")
+            print("Congratulations you have won the game\nThe word is: " + str(guess_word))
+            print("----------------------------------------------------")
+            break
         i += 1
 
 
+# ---------------------------------------------------------------------------------
+
+
+# ------------------------------word checking -------------------------------------
 # This will check the word in put and compare it to the word that has been generated
 def word_check(computer_word, guess_word):
-
-    letters =[]
+    letters = []
     for i in computer_word:
         for j in guess_word:
             if i == j:
@@ -47,18 +63,24 @@ def word_check(computer_word, guess_word):
     return letters_set_like
 
 
+# -------------------------------------------------------------------------------------------------
+
+
+# --------------------------------- Result Confirmation -------------------------------------------
 # this will display the letters required
 
 def correct_word(computer_word, guess_word):
-
     if computer_word == guess_word:
-        print("Congratulations you have won the game\nThe word is: " + str(guess_word))
+        return True
 
+
+# --------------------------------------Play again ------------------------------------------------
 # this will replay the game
-
-
 def play_again():
     pass
+
+
+# --------------------------------------------------------------------------------------------------
 
 
 random_words = ["catch"]
